ae0aa15c30281d9abaa85d363a031ad2206096e9
zarnasavaliya09 <113407633+zarnasavaliya09@users.noreply.github.com>
2023-02-08 19:12:08 -0500

    [fix] complete navigation and add animation

--- Assignment2/js/main.js
+++ Assignment2/js/main.js
@@ -1,22 +1,23 @@
 // WEB303 Assignment 2
 $(document).ready(function(){
-    $('#content-wrapper').on('click', 'a' , function(event) {
-        
+    $("#content-wrapper").on("click", "a", function (event) {
+        var fileToLoad = $(this).attr("id") + ".html";
+        $("a.content").removeClass("content"); // Update links
+        $(this).addClass("content");
         $.ajax({
-        url: this.href,
-        success: function(data) {
-            const $content = $('#content');
-            $content.html(data);
-            $content.removeClass('fadeOutDown');
-            $content.addClass('fadeInUp');
-            $content.show();
-        },
-        error: function() {
-          console.error('Failed to load content');
-        }
+          url: fileToLoad,
+          success: function (data) {
+            const $content = $("#content");
+            $content.html(data).hide().fadeIn(500);
+            $content.removeClass("fadeOutDown");
+            $content.addClass("fadeInUp");
+          },
+          error: function () {
+            console.error("Failed to load content");
+          }
         });
         event.preventDefault();
-    });
+      });
 });
   
 
